89db2f7e9cb51ca01c6eb0c241540d2e511ab428
LucestDail <dhtmdgussksm@naver.com>
2020-07-20 21:07:05 +0900

    commit(add)
    testing, uploading now
    not successful working

---
+++ test.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+function ready() {
+    let overlays = Array.from(document.getElementsByClassName('overlay-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+    for (var i = 0; i < cards.length; i++) {
+				console.dir(cards[i]);
+			}
+    let game = new japanCardGame(100, cards);
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            game.startGame();
+        });
+    });
+    cards.forEach(card =>{
+        card.addEventListener('click',()=>{
+            game.flipCard(card);
+        });
+    });
+}
+
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
