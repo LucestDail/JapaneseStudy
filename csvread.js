9c963942c5405e64b5fb80091718745496f39aa4
hyun <dhtmdgussksm@naver.com>
2020-07-25 17:15:39 +0900

    commit(add)
    add random string creating algorith(stil work on) and add readme.md file

--- csvread.js
+++ csvread.js
@@ -4,7 +4,6 @@ Before Starting
 2. Module jquery.csv.js(min.js) 포함되어 있어야 합니다.
 3. npm module 에 하기 module 이 설치되어야 합니다.
 */
-var comma_separated_values = require('comma-separated-values');
 var fs = require('fs');
 const csv = require('csv-parser')
 
@@ -12,6 +11,9 @@ const urlHiragana = "./doc/Hiragana.csv";
 const urlKatagana = "./doc/Katagana.csv";
 let csvObjectHiragana = [];
 let csvObjectKatagana = [];
+let csvObjectHiraganaRandom = [];
+let csvObjectKataganaRandom = [];
+const MAX_RANDOM_NUMBER = 8;
 
 function readCsv(csvUrl){
     return new Promise((resolve)=>{
@@ -25,11 +27,43 @@ function readCsv(csvUrl){
     })
 }
 
+function createRandomNumber(randomsize){
+    console.log("createRandomNumber 호출됨");
+    console.log("randomsize : " + randomsize);
+    let randomNumber = Math.floor(Math.random()*randomsize);
+    console.log("your random number : " + randomNumber);
+    return randomNumber;
+}
+
+function randomSelect(csvObject){
+    console.log("randomeSelect 함수 호출");
+    return new Promise((resolve) => {
+        let randomSize = Object.keys(csvObject).length;
+        console.log(randomSize);
+        let randomSelectResults = [];
+        for(var i = 0 ; i < MAX_RANDOM_NUMBER ; i++){
+            let randomNumber = createRandomNumber(randomSize);
+            
+            console.log(csvObject[randomNumber]);
+            console.log(i);
+            randomSelectResults[i] = csvObject[randomNumber];
+            console.log("randomSelect : " + randomSelectResults[i]);
+        }
+        return resoleve(randomSelectResults); 
+    });
+}
+
 async function init(){
     csvObjectHiragana = await readCsv(urlHiragana);
     csvObjectKatagana = await readCsv(urlKatagana);
-    console.log(csvObjectHiragana);
-    console.log(csvObjectKatagana);
+    console.log(Object.keys(csvObjectHiragana).length);
+    console.log(typeof csvObjectHiragana);
+    //console.log(csvObjectKatagana);
+
+    csvObjectHiraganaRandom = await randomSelect(csvObjectHiragana);
+    console.log("csvObjectHiraganaRandom : " + csvObjectHiraganaRandom);
+    console.log(typeof csvObjectHiraganaRandom);
+        
 }
 
 init();
--- readfiletest.js
+++
@@ -1,9 +0,0 @@
-var obj = [
-    obj.h = 1,
-    obj.r = 2
-];
-console.log(obj);
-let Obj = new obj();
-obj.push('h1')
-
-console.log(obj);
